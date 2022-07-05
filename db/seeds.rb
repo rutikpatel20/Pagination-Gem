a236840f4ebc756dcdd3c79524f0109937c22a00
rutiksimform <rutik.p@simformsolutions.com>
2022-07-05 11:59:09 +0530

    faker gem add and generate seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  Movie.create(
+    name: Faker::Movie.title,
+    description: Faker::Lorem.paragraph,
+  )
+end
